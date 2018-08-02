df37f74041320771cfdcb28f8f28b927fc5ff7b5
Saveliy Kolesnikov <s.koles33@gmail.com>
2018-08-02 22:30:19 +0300

    Add project files.

---
+++ ChatWithLikes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChatWithLikes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
